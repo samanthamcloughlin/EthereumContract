551528236fd3f90dd872608a96782df525b37e9a
Harvard-Westlake <github@hw.com>
2021-03-08 22:56:46 -0800

    Update to latest Solidity Version + Compile

---
+++ contracts/AdvTopicsContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract AdvTopicsContract {
+
+    // Declare state variables of the contract
+    address public owner;
+    mapping (address => uint) public cupcakeBalances;
+    string public name;
+
+    constructor () {
+        name = "Mr. Theiss";
+    }
+
+    // Sets the public string name
+    function setName (string memory _name) public {
+        name = _name;
+    }
+
+    //
+
+}
+
+// Resources for solidity programming and debugging
+// https://www.bitdegree.org/learn/solidity-examples
+//
